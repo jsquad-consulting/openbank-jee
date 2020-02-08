20d2d4d0693685f65084c584b0067a7a09f9704c
Andy Holst <andy.holst85@gmail.com>
2020-02-08 17:55:53 +0100

    Added Client servlet module
    Added client servlet module to retrieve client information via an
    HttpServlet get webservice operation.
    
    Added an integration test to demonstrate get client information as
    an JSON string via HttpServlet with basic authentication.
    
    Setup web.xml basic authentication roles for the servlet module.
    
    Added integration test to test webservice client to retrieve client
    from the webservice through the ClientToSoapServlet class.
    
    Updated the GetClient.wsdl to have proper soap adress for
    GetClientService for the soap module.
    
    Added webservice environment variables for webservice client used by
    the ClientToSoapServlet class.
    
    Update local and prod setup to include JBOSS admin user and it's
    password environment variables to be used by the basic auth setup
    for the webservice client in the ClientInformationServlet class.
    
    Updated the setup scripts to include the webservice uri and qname
    environment variables.

--- openbank_setup_local.sh
+++ openbank_setup_local.sh
@@ -14,6 +14,13 @@ export OB_USER=$openbank_datasource_obuser_encrypted
 
 export OB_PASSWORD=$openbank_datasource_obpassword_encrypted
 
+export JBOSS_ROOT_USER=$jboss_admin_group_user
+export JBOSS_ROOT_PASSWORD=$jboss_admin_group_password
+
+export WEBSERVICE_WSDL_URL="http://localhost:8080/soap-webservice/GetClientService?wsdl"
+export WEBSERVICE_QURL="http://jsquad.se/"
+export WEBSERVICE_QSERVICE="GetClientWSService"
+
 $WILDFLY_HOME/bin/add-user.sh --silent $jboss_admin_user $jboss_admin_password || true
 $WILDFLY_HOME/bin/add-user.sh -a -g admin --silent $jboss_admin_group_user $jboss_admin_group_password || true
 $WILDFLY_HOME/bin/add-user.sh -a -g customer --silent $jboss_customer_group_user $jboss_customer_group_password || true
